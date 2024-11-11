c1505a76b841f8ebee9600d0d5c70fa68003a917
John M <jm_dk@outlook.com>
2024-11-11 21:10:59 +0100

    read n_ini now almost working

--- src/creation/ini/process_items_ini.cpp
+++ src/creation/ini/process_items_ini.cpp
@@ -129,7 +129,7 @@ bool process_items_ini_C::process_all_structs(info_items_C &items, const std::ve
 						"\t\t\tsize_t pos = line.find('%');\t// remove remainder of line after comment character... \n"
 						"\t\t\tif (pos != std::string::npos)\n"
 						"\t\t\t\t{{\n"
-						"\t\t\t\tline = line.substr(0, pos - 1);\n"
+						"\t\t\t\tline = line.substr(0, pos);\n"
 						"\t\t\t\t}}\n"
 						"\t\t\tif (line.length() == 0)\n"
 						"\t\t\t\tcontinue;\n"
@@ -154,7 +154,7 @@ bool process_items_ini_C::process_all_structs(info_items_C &items, const std::ve
 						"\t\t\t\t\t// space as first character is a \"continue previous line\" thing... (but not implemented currently)\n"
 						"\t\t\t\t\tbreak;\n"
 						"\t\t\t\tdefault:\n"
-						"\t\t\t\t\tregex = \"(\\\\w+) (\\\\w+)\";\n"
+						"\t\t\t\t\tregex = \"(\\\\w+) (.+)\";\n"
 						"\t\t\t\t\tif (std::regex_search(line, res, regex) == true)\n"
 						"\t\t\t\t\t\t{{\n"
 						"\t\t\t\t\t\tstd::string key = res[1].str();\n"
@@ -167,7 +167,7 @@ bool process_items_ini_C::process_all_structs(info_items_C &items, const std::ve
 						"\t\t\t}}\n"
 						"\t\tstd::fclose(file);\n"
 						"\n"
-						"//\t\trv = do_rd_parameters(\"\", &struct_to_fill);\n"
+						"\t\trv = do_rd_{1}(\"\", struct_to_fill);\n"
 						"\t\t}}\n"
 						"\treturn rv;\n"
 						"\t}}\n", reader_class_name, s.name);
@@ -270,7 +270,146 @@ bool process_items_ini_C::process_struct_writer(struct_S const &s, std::string c
 
 bool process_items_n_ini_C::process_struct_reader(struct_S const &s, std::string const class_name, std::FILE *out_file_cpp, std::FILE *out_file_h)
 	{
-	return false;
+	bool rv{ true };
+
+	fmt::println(out_file_h, "\t\tbool do_rd_{0}(std::string path, {0} &data);", s.name);
+
+	fmt::println(out_file_cpp,
+		"bool {0}::do_rd_{1}(std::string path, {1} &data)\n"
+		"\t{{\n"
+		, class_name, s.name);
+	fmt::println(out_file_cpp, "\tbool rv = true;");
+	fmt::println(out_file_cpp, "\ttry");
+	fmt::println(out_file_cpp, "\t\t{{");
+	if (s.simple.empty() == false)
+		{
+		fmt::println(out_file_cpp, "\t\tauto found_items = m_lookup.under.find(path);");
+		fmt::println(out_file_cpp, "\t\tif (found_items != m_lookup.under.cend())");
+		fmt::println(out_file_cpp, "\t\t\t{{");
+		fmt::println(out_file_cpp, "\t\t\tauto items = found_items->second.items;");
+		for (auto &sim : s.simple)
+			{
+			std::string convert{};
+			switch (sim.line_type)
+				{
+				case simple_item_type_E::bool_E:
+					break;
+				case simple_item_type_E::int_E:
+				case simple_item_type_E::unsigned_int_E:
+					convert = "std::stoi";
+					break;
+				case simple_item_type_E::float_E:
+					convert = "std::stof";
+					break;
+				case simple_item_type_E::double_E:
+					convert = "std::stoi";
+					break;
+				case simple_item_type_E::long_E:
+				case simple_item_type_E::short_E:
+				case simple_item_type_E::unsigned_short_E:
+				case simple_item_type_E::std_string_E:
+				case simple_item_type_E::unsigned_long_E:
+					break;
+
+				default:
+					fmt::println(out_file_cpp, "\t// WARNING FAILED TO PROCESS {}\n", sim.name);
+					fmt::println("WARNING : FAILED TO PROCESS {}", sim.name);
+					break;
+				}
+			fmt::println(out_file_cpp,
+				"\t\t\tauto {0} = items.find(\"{0}\");\n"
+				"\t\t\tif({0} != items.cend())\n"
+				"\t\t\t\t{{\n"
+				"\t\t\t\tdata.{0} = {1}({0}->second);\n"
+				"\t\t\t\t}}"
+				, sim.name, convert);
+			}
+		for (auto &co : s.complex)
+			{
+			complex_item_type_E line_type = co.line_type;
+
+			switch (line_type)
+				{
+				case complex_item_type_E::enum_E:
+					fmt::println(out_file_cpp,
+						"\t\t\tauto {0} = items.find(\"{0}\");\n"
+						"\t\t\tif({0} != items.cend())\n"
+						"\t\t\t\t{{\n"
+						"\t\t\t\tstfx::s_to_e(({0}->second, data.{0});\n"
+						"\t\t\t\t}}"
+						, co.name);
+					break;
+				default:
+					break;
+				}
+			}
+		fmt::println(out_file_cpp, "\t\t\t}}");
+		}
+	bool has_children{ false };
+	for (auto &co : s.complex)
+		{
+		complex_item_type_E line_type = co.line_type;
+
+		switch (line_type)
+			{
+			case complex_item_type_E::struct_E:
+			case complex_item_type_E::vector_E:
+				has_children = true;
+				break;
+			default:
+				break;
+			}
+		}
+	if (has_children == true)
+		{
+		fmt::println(out_file_cpp,
+			"\t\tif (path.length() > 0)\n"
+			"\t\t\tpath += \".\";");
+		}
+	if (s.complex.empty() == false)
+		{
+		for (auto &co : s.complex)
+			{
+			complex_item_type_E line_type = co.line_type;
+
+			switch (line_type)
+				{
+				case complex_item_type_E::enum_E:
+					break;
+				case complex_item_type_E::struct_E:
+					fmt::println(out_file_cpp,
+						"\t\trv = do_rd_{1}(path + \"{0}\", data.{0});"
+						, co.name, co.type_name);
+					break;
+				case complex_item_type_E::vector_E:
+					fmt::println(out_file_cpp, "// vector {}\t within structure NOT SUPPORTED by n_ini", co.name);
+					fmt::println("// vector {}\t within structure NOT SUPPORTED by n_ini", co.name);
+					rv = false;
+					break;
+				case complex_item_type_E::enum_or_struct_E:
+					fmt::println(out_file_cpp, "// enum_or_struct_E {}\t NEEDS TO BE FIXED!!", co.name);
+					fmt::println("// enum_or_struct_E {}\t NEEDS TO BE FIXED!!", co.name);
+					rv = false;
+					break;
+				default:
+					fmt::println(out_file_cpp, "\t// WARNING FAILED TO PROCESS {}", co.name);
+					fmt::println("\t// WARNING FAILED TO PROCESS {}", co.name);
+					rv = false;
+					break;
+				}
+			}
+		}
+
+	fmt::println(out_file_cpp, "\t\t}}");
+
+	fmt::println(out_file_cpp, "\tcatch (std::exception &)");
+	fmt::println(out_file_cpp, "\t\t{{");
+	fmt::println(out_file_cpp, "\t\trv = false;");
+	fmt::println(out_file_cpp, "\t\t}}");
+	fmt::println(out_file_cpp,
+		"\treturn rv;\n"
+		"\t}};\n");
+	return rv;
 	}
 
 bool process_items_n_ini_C::process_struct_writer(struct_S const &s, std::string const class_name, std::FILE *out_file_cpp, std::FILE *out_file_h)
@@ -336,12 +475,6 @@ bool process_items_n_ini_C::process_struct_writer(struct_S const &s, std::string
 				break;
 			}
 		}
-#ifdef NOT_NOW
-	if (has_children == true)
-		{
-		fmt::println(out_file_cpp, "\ttinyxml2::XMLElement *ch_el;");
-		}
-#endif
 	for (auto &co : s.complex)
 		{
 		complex_item_type_E line_type = co.line_type;
@@ -368,19 +501,6 @@ bool process_items_n_ini_C::process_struct_writer(struct_S const &s, std::string
 					, co.name, co.type_name);
 				break;
 			case complex_item_type_E::vector_E:
-#ifdef NOT_NOW
-				fmt::println(out_file_cpp,
-					"\tch_el = el->InsertNewChildElement(\"{0}\");\n"
-					"\tif (ch_el != nullptr)\n"
-					"\t\t{{\n"
-					"\t\tfor ({1} itr : data->{0})\n"
-					"\t\t\t{{\n"
-					"\t\t\ttinyxml2::XMLElement *ch_ch_el = ch_el->InsertNewChildElement(\"{1}\");\n"
-					"\t\t\tdo_wr_{1}(ch_ch_el, &itr);\n"
-					"\t\t\t}}\n"
-					"\t\t}}"
-					, co.name, co.type_name);
-#endif
 				fmt::println(out_file_cpp, "// vector {}\t within structure NOT SUPPORTED by n_ini", co.name);
 				fmt::println("// vector {}\t within structure NOT SUPPORTED by n_ini", co.name);
 				rv = false;
